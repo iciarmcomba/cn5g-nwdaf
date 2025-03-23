06d1bf5501061670a6e8505f11569b1ac193b315
iciarmcomba <iciarmartinezcomba@gmail.com>
2025-03-23 21:28:43 +0100

    Ajustes para el nuevo dataset brain-tumor

--- oai-cn5g-nwdaf/docker-compose/init-db.js
+++ oai-cn5g-nwdaf/docker-compose/init-db.js
@@ -2,6 +2,12 @@
 db = db.getSiblingDB('yolov8');
 
 // Insertamos el conjunto de datos
-db.coco8.insertMany([
-  { "dataset": "coco8", "images_path": "/coco8/images", "labels_path": "/coco8/labels" }
-]);
+//db.coco8.insertMany([
+//  { "dataset": "coco8", "images_path": "/coco8/images", "labels_path": "/coco8/labels" }
+//]);
+
+db["brain-tumor"].insertOne({
+  dataset: "brain-tumor",
+  images_path: "/brain-tumor/train/images",
+  labels_path: "/brain-tumor/train/labels"
+});
